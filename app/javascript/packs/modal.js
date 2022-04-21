5d2d1b64aa3ff290a1f5e26963bcd55de0c90598
Isaac Osuna <eddieisaac@gmail.com>
2022-04-20 22:41:15 -0600

    Renderizado dinamico de modal por categoria

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', init)
 
 let productData = []
-let selectedCategoryIndex = 0
 
 async function fetchData(){
     let url = "https://gist.githubusercontent.com/LuigiR0jas/debdf41add7704681d9f7c734f478ac2/raw/7e7e5ad88132640d659c3ec8e657d8e02af01aa7/crates.json"
@@ -15,11 +14,56 @@ async function fetchData(){
     })
 }
 
+function loadCategory(category) {
+    let img = document.getElementById('category-img')
+    let title = document.getElementById('category-title')
+    let description = document.getElementById('category-description')
+    let items = document.getElementById('items')
+
+    img.src = category.picture
+    title.innerText = category.name
+    description.innerText = category.description
+
+    let tea,name, picture, desc, rating
+
+    items.innerText = ''
+
+    category.content.forEach((item) => {
+        tea = document.createElement('div')
+        tea.classList.add('item-card')
+
+        picture = document.createElement('img')
+        picture.src = item.picture
+        picture.classList.add('item-img')
+
+        name = document.createElement('span')
+        name.innerText = item.name
+        name.classList.add('item-name')
+
+        desc = document.createElement('span')
+        desc.innerText = item.description
+        desc.classList.add('item-desc')
+
+        rating = document.createElement('span')
+        rating.innerText = item.rating + ' ☆'
+        rating.classList.add('item-rating')
+
+        tea.appendChild(picture)
+        tea.appendChild(name)
+        tea.appendChild(desc)
+        tea.appendChild(rating)
+        items.appendChild(tea)
+    })
+}
+
 function renderCategoryList(){
     let modalElement = document.getElementById('categories')
     let category, img, name, rating, div
 
+    modalElement.innerText = ''
+
     productData.forEach((item, index) => {
+        console.log(item)
         category = document.createElement('div')
         category.classList.add('category-card')
         category.id = item.crate_id
@@ -39,13 +83,19 @@ function renderCategoryList(){
         rating.classList.add('category-rating')
         rating.innerText = item.rating + ' ☆'
 
-
         div.appendChild(name)
         div.appendChild(rating)
         category.appendChild(img)
         category.appendChild(div)
+
+        category.addEventListener('click', (e) => {
+            loadCategory(item)
+        })
+
         modalElement.appendChild(category)
     })
+
+    loadCategory(productData[0])
 }
 
 function init () {
